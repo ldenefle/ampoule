3b38f1a2989f7a56595e61250dc41710f57e57d7
Lucas Denefle <lucas.denefle@converge.io>
2023-12-16 12:54:56 +0000

    chore: add clang-format + github

--- include/ampoule/ingestion.h
+++ include/ampoule/ingestion.h
@@ -22,43 +22,40 @@ extern "C" {
 /******************************************************************************/
 /* Global Definitions/Macros                                                  */
 /******************************************************************************/
-#define INGESTION_PACKET_MAX_SIZE   1024
-#define INGESTION_OBSERVERS_MAX_SIZE   3
-
+#define INGESTION_PACKET_MAX_SIZE    1024
+#define INGESTION_OBSERVERS_MAX_SIZE 3
 
 /******************************************************************************/
 /* External Typedefs                                                          */
 /******************************************************************************/
-typedef int (*on_data_cb)(uint8_t * data, uint16_t len);
+typedef int (*on_data_cb)(uint8_t *data, uint16_t len);
 
 enum ingestion_state {
-    RCV_LENGTH_HIGH,
-    RCV_LENGTH_LOW,
-    RCV_DATA,
+	RCV_LENGTH_HIGH,
+	RCV_LENGTH_LOW,
+	RCV_DATA,
 };
 
 struct ingestion {
-    /* Used by sys work q */
-    struct k_work ingest_work;
-    struct k_work_delayable timeout_work;
+	/* Used by sys work q */
+	struct k_work ingest_work;
+	struct k_work_delayable timeout_work;
 
-    struct ring_buf rb;
+	struct ring_buf rb;
 
-    enum ingestion_state state;
+	enum ingestion_state state;
 
-    uint8_t current[INGESTION_PACKET_MAX_SIZE];
-    uint16_t expected_size;
-    uint16_t bytes_read;
+	uint8_t current[INGESTION_PACKET_MAX_SIZE];
+	uint16_t expected_size;
+	uint16_t bytes_read;
 
-    /* Observers list, can sub to packet received */
-    on_data_cb observer;
+	/* Observers list, can sub to packet received */
+	on_data_cb observer;
 };
 
 /******************************************************************************/
 /* External Variables                                                         */
 /******************************************************************************/
-
-
 
 /******************************************************************************/
 /* Global Function Prototypes                                                 */
@@ -68,16 +65,12 @@ struct ingestion {
  * @params <++>
  * @return <++>
  */
-int ingestion_init(struct ingestion * ingestion, on_data_cb cb);
+int ingestion_init(struct ingestion *ingestion, on_data_cb cb);
 
-int ingestion_feed(struct ingestion * ingestion, uint8_t * data, uint16_t len);
-
-
+int ingestion_feed(struct ingestion *ingestion, uint8_t *data, uint16_t len);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* INGESTION */
-
-
--- tests/ingestion/src/main.c
+++ tests/ingestion/src/main.c
@@ -6,18 +6,15 @@
  *
  */
 
-
 /******************************************************************************/
 /* Includes                                                                   */
 /******************************************************************************/
 #include <zephyr/ztest.h>
 #include "ampoule/ingestion.h"
 
-
 /******************************************************************************/
 /* Local Constant, Macro and Type Definitions                                 */
 /******************************************************************************/
-
 
 /******************************************************************************/
 /* Local Function Prototypes                                                  */
@@ -32,53 +29,60 @@ static bool cb_called = false;
 /******************************************************************************/
 /* Global Function Definitions                                                */
 /******************************************************************************/
-static int cb(uint8_t * data, uint16_t len){
-    cb_called = true;
-    return 0;
+static int cb(uint8_t *data, uint16_t len)
+{
+	cb_called = true;
+	return 0;
 }
 
-static void before(void * fixture){
-    cb_called = false;
+static void before(void *fixture)
+{
+	cb_called = false;
 }
 
-ZTEST(in_tests, test_ingestion_NULL_cb_trigger_error) {
-    zassert_true(ingestion_init(&ingestion, NULL) == -EINVAL);
+ZTEST(in_tests, test_ingestion_NULL_cb_trigger_error)
+{
+	zassert_true(ingestion_init(&ingestion, NULL) == -EINVAL);
 }
 
-ZTEST(in_tests, test_ingestion_non_null_cb_trigger_success) {
-    zassert_ok(ingestion_init(&ingestion, cb));
+ZTEST(in_tests, test_ingestion_non_null_cb_trigger_success)
+{
+	zassert_ok(ingestion_init(&ingestion, cb));
 }
 
-ZTEST(in_tests, test_ingestion_can_receive_pkt) {
-    zassert_ok(ingestion_init(&ingestion, cb));
-    uint8_t packet[3] = {0, 0x01, 0};
+ZTEST(in_tests, test_ingestion_can_receive_pkt)
+{
+	zassert_ok(ingestion_init(&ingestion, cb));
+	uint8_t packet[3] = {0, 0x01, 0};
 
-    ingestion_feed(&ingestion, packet, sizeof(packet));
+	ingestion_feed(&ingestion, packet, sizeof(packet));
 
-    k_sleep(K_MSEC(1));
-    zassert_true(cb_called);
+	k_sleep(K_MSEC(1));
+	zassert_true(cb_called);
 }
 
-ZTEST(in_tests, test_ingestion_uncomplete_packet_timeout) {
-    zassert_ok(ingestion_init(&ingestion, cb));
-    uint8_t garbage[3] = {0, 0x01, 0x02};
+ZTEST(in_tests, test_ingestion_uncomplete_packet_timeout)
+{
+	zassert_ok(ingestion_init(&ingestion, cb));
+	uint8_t garbage[3] = {0, 0x01, 0x02};
 
-    ingestion_feed(&ingestion, garbage, sizeof(garbage));
+	ingestion_feed(&ingestion, garbage, sizeof(garbage));
 
-    zassert_false(cb_called);
+	zassert_false(cb_called);
 }
 
-ZTEST(in_tests, test_ingestion_timeout_allow_for_resending) {
-    zassert_ok(ingestion_init(&ingestion, cb));
-    uint8_t garbage[3] = {0, 0x01, 0x02};
+ZTEST(in_tests, test_ingestion_timeout_allow_for_resending)
+{
+	zassert_ok(ingestion_init(&ingestion, cb));
+	uint8_t garbage[3] = {0, 0x01, 0x02};
 
-    ingestion_feed(&ingestion, garbage, sizeof(garbage));
+	ingestion_feed(&ingestion, garbage, sizeof(garbage));
 
-    k_sleep(K_MSEC(500));
-    uint8_t packet[3] = {0, 0x01, 0};
+	k_sleep(K_MSEC(500));
+	uint8_t packet[3] = {0, 0x01, 0};
 
-    ingestion_feed(&ingestion, packet, sizeof(packet));
-    zassert_true(cb_called);
+	ingestion_feed(&ingestion, packet, sizeof(packet));
+	zassert_true(cb_called);
 }
 
 /******************************************************************************/
